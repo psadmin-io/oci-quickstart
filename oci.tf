1b0242ed80c5e01bb5383e8775427d2e95264087
Dan Iverson <dan@daniverson.com>
2018-09-19 16:55:20 -0500

    Add security lists to subnet

--- oci.tf
+++ oci.tf
@@ -97,9 +97,12 @@ resource "oci_core_subnet" "sandbox-subnet" {
   // availability_domain = "${lookup(data.oci_identity_availability_domains.primary_availability_domains.availability_domains[count.index],"name")}"
   availability_domain = "${var.availability_domain}"
   cidr_block          = "10.0.${count.index}.0/24"
-  display_name        = "${oci_core_vcn.sandbox_vcn.display_name}-subnet${count.index}-sandbox"
+  display_name        = "${oci_core_vcn.sandbox_vcn.display_name} Subnet ${count.index}"
   // dns_label           = "subnet${count.index}sandbox"
-  security_list_ids   = ["${oci_core_security_list.sandbox_nfs_security_list.id}"]
+  security_list_ids   = [
+    "${oci_core_security_list.sandbox_nfs_security_list.id}",
+    "${oci_core_security_list.sandbox_midtier_security_list.id}",
+    "${oci_core_security_list.sandbox_db_security_list.id}" ]
   compartment_id      = "${oci_identity_compartment.sandbox_compartment.id}"
   vcn_id              = "${oci_core_vcn.sandbox_vcn.id}"
   route_table_id      = "${oci_core_route_table.sandbox_route_table.id}"
