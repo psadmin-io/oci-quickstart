5fc1845c0b65fc68c181c214b5473e275d24f63b
Dan Iverson <dan@daniverson.com>
2018-09-19 13:18:26 -0500

    Change cidr_block to destination for route_table

--- oci.tf
+++ oci.tf
@@ -68,7 +68,7 @@ resource "oci_core_route_table" "sandbox_route_table" {
   vcn_id         = "${oci_core_vcn.sandbox_vcn.id}"
   
   route_rules {
-    cidr_block        = "0.0.0.0/0"
+    destination        = "0.0.0.0/0"
     network_entity_id = "${oci_core_internet_gateway.sandbox_internet_gateway.id}"
   }
 }
